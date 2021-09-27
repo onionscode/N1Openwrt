4df994d695c6b8e7c0d24ca8d932f7405d729855
onionscode <575968661@qq.com>
2021-09-27 19:38:16 +0800

    Update diy-ARMv8.sh

--- diy-ARMv8.sh
+++ diy-ARMv8.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.10.2/10.10.10.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.10.2/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Hostname
 sed -i 's/OpenWrt/qnmlgb/g' package/base-files/files/bin/config_generate
