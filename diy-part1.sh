f3c0be02154a86463fdf3356e31a3f106b90f08a
onionscode <575968661@qq.com>
2021-10-20 09:32:07 +0800

    x86

---
+++ diy-part1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+sed -i '$a src-git mosdns https://github.com/Gzxhwq/openwrt-mos;dev' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git ssrp https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+
+#Add external packages
+#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/others/luci-app-openclash
+#git submodule add https://github.com/destan19/OpenAppFilter.git package/others/OpenAppFilter
+#git submodule add https://github.com/tty228/luci-app-serverchan.git package/others/luci-app-serverchan
+#git submodule add -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/others/luci-theme-edge
+#git submodule add https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/others/luci-theme-infinityfreedom
+#git submodule add https://github.com/garypang13/luci-app-eqos.git package/others/luci-app-eqos
+#git submodule add https://github.com/lisaac/luci-app-dockerman.git package/others/luci-app-dockerman
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/others/luci-app-amlogic
+
---
+++ diy-part2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.100/g' package/base-files/files/bin/config_generate
+
+# Revert to official distfeeds
+#sed -i '/ustclug/d' package/lean/default-settings/files/zzz-default-settings
+
+#Apply the patches
+git apply $GITHUB_WORKSPACE/patches/*.patch
+
+#Fix qemu-ga shutdown issue
+#cd ./feeds/packages/
+#git apply $GITHUB_WORKSPACE/feeds-patches/*.patch
+#cd ../../
+
+# Update mwan3helper's IP pools
+wget https://ispip.clang.cn/all_cn_cidr.txt -O package/lean/luci-app-mwan3helper/root/etc/mwan3helper/all_cn.txt
+wget https://ispip.clang.cn/chinatelecom_cidr.txt -O package/lean/luci-app-mwan3helper/root/etc/mwan3helper/chinatelecom.txt
+wget https://ispip.clang.cn/unicom_cnc_cidr.txt -O package/lean/luci-app-mwan3helper/root/etc/mwan3helper/unicom_cnc.txt
+wget https://ispip.clang.cn/cmcc_cidr.txt -O package/lean/luci-app-mwan3helper/root/etc/mwan3helper/cmcc.txt
+wget https://ispip.clang.cn/crtc_cidr.txt -O package/lean/luci-app-mwan3helper/root/etc/mwan3helper/crtc.txt
+wget https://ispip.clang.cn/cernet_cidr.txt -O package/lean/luci-app-mwan3helper/root/etc/mwan3helper/cernet.txt
+wget https://ispip.clang.cn/gwbn_cidr.txt -O package/lean/luci-app-mwan3helper/root/etc/mwan3helper/gwbn.txt
+wget https://ispip.clang.cn/othernet_cidr.txt -O package/lean/luci-app-mwan3helper/root/etc/mwan3helper/othernet.txt
+
+# Change dnsproxy behave
+sed -i 's/--cache --cache-min-ttl=3600/--cache --cache-min-ttl=600/g' ./package/lean/luci-app-turboacc/root/etc/init.d/turboacc
