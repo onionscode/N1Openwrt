7a5e6575dcc1d4238542a6f9971db5d613253362
onionscode <575968661@qq.com>
2022-07-08 16:42:09 +0800

    build-onecloud

---
+++ diy-onecloud2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.10.5/g' package/base-files/files/bin/config_generate
+
+
+echo '修改机器名称'
+sed -i 's/OpenWrt/Onecloud/g' package/base-files/files/bin/config_generate
+
+# Modify default IP
+sed -i 's/192.168.31.1/192.168.31.2/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
